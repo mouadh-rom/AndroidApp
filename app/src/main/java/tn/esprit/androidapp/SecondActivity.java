dfa205be0793de83ce35385fd8b82b0572a260e1
romdhane.moadh <moadh.romdhane@esprit.tn>
2022-11-17 10:44:14 +0100

    first commit

---
+++ app/src/main/java/tn/esprit/androidapp/SecondActivity.java
@@ -0,0 +1,69 @@
+package tn.esprit.androidapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class SecondActivity extends AppCompatActivity {
+GoogleSignInOptions gso;
+GoogleSignInClient gsc;
+TextView name,email;
+Button signOutBtn ;
+Button continubtn;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        name = findViewById(R.id.name);
+        email = findViewById(R.id.email);
+        signOutBtn = findViewById(R.id.signout);
+        continubtn = findViewById(R.id.continu);
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(this,gso);
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+        if(acct!=null){
+            String personName = acct.getDisplayName();
+            String personEmail = acct.getEmail();
+            name.setText(personName);
+            email.setText(personEmail);
+        }
+        continubtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SecondActivity.this,ThirdActivity.class);
+           startActivity(intent);
+            }
+        });
+        signOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOut();
+            }
+        });
+    }
+
+
+    void signOut(){
+        gsc.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                finish();
+                startActivity(new Intent(SecondActivity.this,MainActivity.class));
+            }
+        });
+   }
+}
